8bbd2c3076edbdf0c8e63f350a9b42322d9b0be9
Prarthana Purohit <prarthana.purohit03@gmail.com>
2025-12-21 23:47:31 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+//request handler
+app.use("/test", (req, res) => {
+    res.send("Hello from Express!");
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");  
+});
